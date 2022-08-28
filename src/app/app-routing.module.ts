58dd5ccc1f67b5ed6195b3033167fc56bd98ca7b
Rushikesh Shinde <10303320171124611002@dbatu.ac.in>
2022-08-28 18:39:22 +0530

    change favicon, add routing, import models, update component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CartComponent} from "./component/cart/cart.component";
+import {ProductsComponent} from "./component/products/products.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'cart' , component : CartComponent},
+  {path : 'product', component : ProductsComponent},
+  {path : '', redirectTo : 'product', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
